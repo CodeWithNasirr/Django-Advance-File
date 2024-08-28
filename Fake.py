8c8749e997dc420eb2adb7436f69afe54ca1b98c
CodeWithNasirr <skofficial665@gmail.com>
2024-08-29 02:32:04 +0530

    29aug

--- Fake.py
+++ Fake.py
@@ -27,3 +27,28 @@ def xyzx(record=10)->None:
 #         pub=random.choice(pubs)
 #         book.publisher = pub
 #         book.save()
+
+
+
+# Bulk Create And Delete and Update
+import time
+def create_Cat(number):
+    start_time = time.time()
+    create = []
+    for _ in range(number):
+        create.append(Category(name=fake.name()))
+    Category.objects.bulk_create(create)
+    end_time = time.time()
+    print(f"Created {number} categories in {end_time - start_time:.2f} seconds.")
+
+create_Cat(100000)
+
+
+
+def delete():
+    Category.objects.all().delete()
+
+def update(name):
+    print(Category.objects.filter(name__icontains=name).count())
+    print(Category.objects.filter(name__icontains=name).update(name='FARHA'))
+update('Agrawal')
