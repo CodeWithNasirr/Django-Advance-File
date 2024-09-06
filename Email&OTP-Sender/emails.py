87941346e0edf4496d28914d2889b71ba268a10c
CodeWithNasirr <skofficial665@gmail.com>
2024-09-06 19:43:20 +0530

    6sep

--- Email&OTP-Sender/emails.py
+++ Email&OTP-Sender/emails.py
@@ -11,12 +11,3 @@ def Send_Otp(email,subject,message):
     send_mail(subject,message,settings.EMAIL_HOST_USER,[email])
 
 
-#Setting.py
-
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-
-# EMAIL_HOST_USER="skofficial665@gmail.com"
-# EMAIL_HOST_PASSWORD="fast qkcx sgli haus"
